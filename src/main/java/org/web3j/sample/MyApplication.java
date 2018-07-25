39c7e351fc4c41f8715009d38f68fcf9061f6eb6
Denis Timofeev <nakolenke@gmail.com>
2018-07-26 00:07:02 +0300

    Send funds using raw transaction

--- src/main/java/org/web3j/sample/MyApplication.java
+++ src/main/java/org/web3j/sample/MyApplication.java
@@ -3,19 +3,22 @@ package org.web3j.sample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.web3j.crypto.Credentials;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.crypto.TransactionEncoder;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.sample.contracts.generated.AuthorizedContract;
-import org.web3j.tx.Contract;
-import org.web3j.tx.ManagedTransaction;
-import org.web3j.tx.Transfer;
 import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
-import java.math.BigDecimal;
-
-import static org.web3j.utils.Convert.Unit.GWEI;
+import java.math.BigInteger;
+import java.util.Optional;
 
 /**
  * A simple web3j application that demonstrates a number of core features of web3j:
@@ -45,9 +48,19 @@ import static org.web3j.utils.Convert.Unit.GWEI;
  */
 public class MyApplication {
 
+    public static final BigInteger GAS_PRICE = BigInteger.valueOf(22_000_000_000L);
+    public static final BigInteger GAS_LIMIT = BigInteger.valueOf(4_300_000);
+    private static final int SLEEP_DURATION = 15000;
+    private static final int ATTEMPTS = 40;
     private static final Logger log = LoggerFactory.getLogger(MyApplication.class);
     private String gatewayUrl = "https://rinkeby.infura.io/VIhu8Q3dA5fy5NbxDLjR";
-//    private String gatewayUrl = "http://localhost:7545";
+    private String credentialsFilePath = "/home/denis/.ethereum/rinkeby/keystore/UTC--2018-07-15T14-29-16.262544014Z--93fcb1de0a0fef8528950043c3459641146203d3";
+    private String wallet = "0x93fcb1de0a0fef8528950043c3459641146203d3";
+    private String password = "1q2w#E$R";
+    private Web3j web3j;
+    private String contractAddress = "0xdCAB085368563516222740A336b29A6Ea998237E";
+
+    //    private String gatewayUrl = "http://localhost:7545";
 
 
     public static void main(String[] args) throws Exception {
@@ -57,60 +70,94 @@ public class MyApplication {
     private void run() throws Exception {
 
         // We start by creating a new web3j instance to connect to remote nodes on the network.
-        // Note: if using web3j Android, use Web3jFactory.build(...
-        Web3j web3j = Web3j.build(new HttpService(gatewayUrl));
+        web3j = Web3j.build(new HttpService(gatewayUrl));
         log.info("Connected to Ethereum client version: " + web3j.web3ClientVersion().send().getWeb3ClientVersion());
 
         // We then need to load our Ethereum wallet file
-        Credentials credentials =
-                WalletUtils.loadCredentials(
-                        "1q2w#E$R",
-                        "/home/denis/.ethereum/rinkeby/keystore/UTC--2018-07-15T14-29-16.262544014Z--93fcb1de0a0fef8528950043c3459641146203d3");
+        Credentials credentials = WalletUtils.loadCredentials(password, credentialsFilePath);
         log.info("Credentials loaded");
 
-        // Now lets deploy a smart contract
-//        log.info("Deploying smart contract");
-//        AuthorizedContract contract = AuthorizedContract.deploy(
-//                web3j, credentials,
-//                ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT).send();
-//
-//        String contractAddress = contract.getContractAddress();
-        String contractAddress = "0xdCAB085368563516222740A336b29A6Ea998237E";
-        AuthorizedContract contract = AuthorizedContract.load(contractAddress, web3j, credentials,
-                ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT);
-        log.info("Smart contract address " + contractAddress);
-        log.info("View contract at https://rinkeby.etherscan.io/address/" + contractAddress);
+        // AuthorizedContract contract = AuthorizedContract.load(contractAddress, web3j, credentials,
+        //         ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT);
+        // log.info("Smart contract address " + contractAddress);
+        // log.info("View contract at https://rinkeby.etherscan.io/address/" + contractAddress);
 
-//        contract.setGasPrice(BigInteger.valueOf(30000000L));
-
-//        log.info("Authorizing Ethereum wallet...");
-//        contract.addAddress("0x93fcb1de0a0fef8528950043c3459641146203d3").send();
-//        log.info("Done!");
+        // contract.setGasPrice(BigInteger.valueOf(30000000L));
+        // log.info("Authorizing Ethereum wallet...");
+        // contract.addAddress("0x93fcb1de0a0fef8528950043c3459641146203d3").send();
+        // log.info("Done!");
 
         // Send some wei to the contract
-        log.info("Sending 32000 GWei (" + Convert.fromWei("32000000000000", Convert.Unit.ETHER).toPlainString() + " to contract");
+        BigInteger nonce = getNonce(wallet);
+        BigInteger value = Convert.toWei("32000", Convert.Unit.GWEI).toBigInteger();
 
-        TransactionReceipt transferReceipt = Transfer.sendFunds(
-                web3j, credentials, contractAddress, BigDecimal.valueOf(32000), GWEI)
-                .send();
-        log.info("Transaction complete, view it at https://rinkeby.etherscan.io/tx/"
-                + transferReceipt.getTransactionHash());
+        Transaction transaction = Transaction.createEtherTransaction(
+                wallet, nonce, GAS_PRICE, GAS_LIMIT, contractAddress, value);
 
-//         Lets modify the value in our smart contract
-//        TransactionReceipt transactionReceipt = contract..newGreeting("Well hello again").send();
 
-//        log.info("New value stored in remote smart contract: " + contract.greet().send());
+        RawTransaction rawTransaction = RawTransaction.createEtherTransaction(
+                nonce, GAS_PRICE, GAS_LIMIT, contractAddress, value);
+        byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+        String signHexValue = Numeric.toHexString(signedMessage);
+        EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(signHexValue).sendAsync().get();
 
-        // Events enable us to log specific events happening during the execution of our smart
-        // contract to the blockchain. Index events cannot be logged in their entirety.
-        // For Strings and arrays, the hash of values is provided, not the original value.
-        // For further information, refer to https://docs.web3j.io/filters.html#filters-and-events
+        String transactionHash = ethSendTransaction.getTransactionHash();
 
-//        for (Greeter.ModifiedEventResponse event : contract.getModifiedEvents(transactionReceipt)) {
-//            log.info("Modify event fired, previous value: " + event.oldGreeting
-//                    + ", new value: " + event.newGreeting);
-//            log.info("Indexed event previous value: " + Numeric.toHexString(event.oldGreetingIdx)
-//                    + ", new value: " + Numeric.toHexString(event.newGreetingIdx));
-//        }
+        // assertFalse(transactionHash.isEmpty());
+
+        TransactionReceipt transactionReceipt = waitForTransactionReceipt(transactionHash);
+        log.info("Transaction receipt is: " + transactionReceipt.getTransactionHash());
+
+        // assertThat(transactionReceipt.getTransactionHash(), is(transactionHash));
+
+//        log.info("Sending 32000 GWei (" + Convert.fromWei("32000000000000", Convert.Unit.ETHER).toPlainString() + " to contract");
+//
+//        TransactionReceipt transferReceipt = Transfer.sendFunds(
+//                web3j, credentials, contractAddress, BigDecimal.valueOf(32000), GWEI)
+//                .send();
+//        log.info("Transaction complete, view it at https://rinkeby.etherscan.io/tx/"
+//                + transferReceipt.getTransactionHash());
+
+    }
+
+    public BigInteger getNonce(String address) throws Exception {
+        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(
+                address, DefaultBlockParameterName.LATEST).sendAsync().get();
+
+        return ethGetTransactionCount.getTransactionCount();
+    }
+
+    TransactionReceipt waitForTransactionReceipt(
+            String transactionHash) throws Exception {
+
+        Optional<TransactionReceipt> transactionReceiptOptional =
+                getTransactionReceipt(transactionHash, SLEEP_DURATION, ATTEMPTS);
+
+        if (!transactionReceiptOptional.isPresent()) {
+            throw new RuntimeException("Transaction receipt not generated after " + ATTEMPTS + " attempts");
+        }
+
+        return transactionReceiptOptional.get();
+    }
+
+    private Optional<TransactionReceipt> getTransactionReceipt(
+            String transactionHash, int sleepDuration, int attempts) throws Exception {
+
+        Optional<TransactionReceipt> receiptOptional = sendTransactionReceiptRequest(transactionHash);
+        for (int i = 0; i < attempts; i++) {
+            if (!receiptOptional.isPresent()) {
+                Thread.sleep(sleepDuration);
+                receiptOptional = sendTransactionReceiptRequest(transactionHash);
+            } else {
+                break;
+            }
+        }
+
+        return receiptOptional;
+    }
+
+    private Optional<TransactionReceipt> sendTransactionReceiptRequest(String transactionHash) throws Exception {
+        EthGetTransactionReceipt transactionReceipt = web3j.ethGetTransactionReceipt(transactionHash).sendAsync().get();
+        return transactionReceipt.getTransactionReceipt();
     }
 }
